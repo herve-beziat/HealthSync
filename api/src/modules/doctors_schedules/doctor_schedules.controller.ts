db9ca21b7b7dc0cdae88544c7eda37b438fea1dd
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2026-07-18 22:28:21 +0200

    feat: add appointments CRUD

--- api/src/modules/doctors_schedules/doctor_schedules.controller.ts
+++ api/src/modules/doctors_schedules/doctor_schedules.controller.ts
@@ -2,6 +2,7 @@ import { Context } from "hono";
 import { DoctorScheduleSchema } from "./doctor_schedules.dto.js";
 import * as scheduleService from "./doctor_schedules.service.js";
 import { Prisma } from "@prisma/client";
+import { grpcScheduleClient } from "../../clients/schedule.client.js";
 
 export const getSchedulesByDoctor = async (c: Context) => {
   const doctorId = c.req.param("doctorId");
@@ -72,5 +73,60 @@ export const deleteSchedule = async (c: Context) => {
       return c.json({ error: "Schedule not found" }, 404);
     }
     return c.json({ error: "Failed to delete schedule" }, 500);
+  }
+};
+
+export const getSlotsByDoctorId = async (c: Context) => {
+  const doctorId = c.req.param("doctorId");
+  const date = c.req.query("date");
+
+  if (!doctorId || !date) {
+    return c.json({ error: "doctorId and date query parameters are required" }, 400);
+  }
+
+  try {
+
+    const response = await new Promise((resolve, reject) => {
+      grpcScheduleClient.getDoctorSchedule({ doctorId, date }, (err: any, res: any) => {
+        if (err) return reject(err);
+        resolve(res);
+      });
+    });
+
+    // On renvoie la réponse JSON une fois la Promise résolue
+    return c.json(response, 200);
+  } catch (error) {
+    console.error("Error fetching doctor schedule:", error);
+    return c.json({ error: "Failed to retrieve doctor schedule" }, 500);
+  }
+};
+
+export const getAvailableSlots = async (c: Context) => {
+  const doctorId = c.req.param("doctorId");
+  const date = c.req.query("date");
+
+  if (!date) {
+    return c.json({ error: "Query parameter 'date' is required" }, 400);
+  }
+
+  try {
+    const gRPCResponse = await new Promise((resolve, reject) => {
+      grpcScheduleClient.getDoctorAvailableSlots(
+        { doctorId, date },
+        (err: any, response: any) => {
+          if (err) return reject(err);
+          resolve(response);
+        }
+      );
+    });
+
+    return c.json(gRPCResponse, 200);
+
+  } catch (error: any) {
+    console.error("gRPC Client Error:", error);
+    return c.json({ 
+      error: "Failed to fetch available slots from internal service",
+      details: error.details || error.message 
+    }, 500);
   }
 };
