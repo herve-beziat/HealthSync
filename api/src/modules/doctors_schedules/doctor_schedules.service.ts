052da397bc7b9a52f0023c00ff3f9dcc502bb600
herve-beziat <herve.beziat@laplateforme.io>
2026-07-22 19:06:51 +0200

    test(doctors_schedules): export and unit test parseTimeToDate

--- api/src/modules/doctors_schedules/doctor_schedules.service.ts
+++ api/src/modules/doctors_schedules/doctor_schedules.service.ts
@@ -1,7 +1,7 @@
 import { doctor_schedules } from "@prisma/client";
 import { prisma } from "../../utils/prisma.js";
 
-const parseTimeToDate = (timeString: string): Date => {
+export const parseTimeToDate = (timeString: string): Date => {
   const [hours, minutes] = timeString.split(":").map(Number);
   const date = new Date();
   date.setHours(hours, minutes, 0, 0);
